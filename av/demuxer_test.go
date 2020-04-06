47876e75b508819acc0302bcbd7b9b444e5a4547
Ben Ji <bin.go2011@yahoo.com>
2020-04-06 14:36:49 +0800

    TestAudioDecoding

--- av/demuxer_test.go
+++ av/demuxer_test.go
@@ -1,17 +1,12 @@
 package av
 
 import (
-	"fmt"
-	"os"
 	"testing"
 )
 
-func TestDemuxingAndDecoding(t *testing.T) {
+func TestAudioDecoding(t *testing.T) {
 	demuxer, _ := NewDemuxer("big_buck_bunny.mp4")
 	defer demuxer.Release()
-
-	f, _ := os.Create("big_buck_bunny.pcm")
-	defer f.Close()
 
 	adec, err := demuxer.OpenAudioDecoder()
 	if err != nil {
@@ -26,24 +21,11 @@ func TestDemuxingAndDecoding(t *testing.T) {
 	frame, _ := NewAVFrame()
 	defer frame.Release()
 
-	for {
-		n := demuxer.ReadFrame(pkt)
-		if n < 0 {
-			break
-		}
-		// fmt.Printf("%#v\n", pkt)
-		if pkt.stream_index == demuxer.AudioStreamIndex {
-			err := adec.Decode(frame, &got_frame, pkt)
-			if err != nil {
-				t.Error(err)
-			}
-			// fmt.Printf("%#v\n", frame.CAVFrame)
-
-			if got_frame > 0 {
-				f.Write(frame.Data)
-			}
+	demuxer.ReadFrame(pkt)
+	if pkt.stream_index == demuxer.AudioStreamIndex {
+		err := adec.Decode(frame, &got_frame, pkt)
+		if err != nil {
+			t.Error(err)
 		}
 	}
-
-	fmt.Println("Done")
 }
