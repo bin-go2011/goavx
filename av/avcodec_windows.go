ffae2536baf9bd2c82a793b50c6b0f6da5fca772
ben <bin.go2011@yahoo.com>
2020-04-09 23:41:33 +0800

    remove avpacket

--- av/avcodec_windows.go
+++ av/avcodec_windows.go
@@ -9,6 +9,56 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+type CAVPacket struct {
+	buf             uintptr
+	pts             int64
+	dts             int64
+	data            uintptr
+	size            int32
+	stream_index    int32
+	flags           int32
+	side_data       uintptr
+	side_data_elems int32
+	duration        int64
+	pos             int64
+}
+type AVPacket struct {
+	CAVPacket
+	handle uintptr
+	Data   []byte
+}
+
+var (
+	avAllocPacketProc,
+	avFreePacketProc *windows.Proc
+)
+
+func AvAllocPacket() (*AVPacket, error) {
+	if avAllocPacketProc == nil {
+		avAllocPacketProc = goavx.LoadedDLL.MustFindProc("_av_packet_alloc")
+	}
+
+	r1, _, err := avAllocPacketProc.Call()
+
+	if r1 == 0 {
+		return nil, err
+	}
+
+	pkt := &AVPacket{
+		handle: r1,
+	}
+
+	return pkt, nil
+}
+
+func AvFreePacket(pkt *AVPacket) {
+	if avFreePacketProc == nil {
+		avFreePacketProc = goavx.LoadedDLL.MustFindProc("_av_free_packet")
+	}
+
+	avFreePacketProc.Call(pkt.handle)
+}
+
 type AVCodecContext struct {
 	handle uintptr
 }
